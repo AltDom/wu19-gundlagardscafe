5dd9947dc0775baacf2fd74d0feb52cc8981c61c
Dominic Kersch <kerschdominic@gmail.com>
2020-09-08 16:41:29 +0200

    create pages

--- web/client.js
+++ web/client.js
@@ -1,7 +1,7 @@
-import sanityClient from "@sanity/client";
+import sanityClient from '@sanity/client';
 
 export default sanityClient({
-  projectId: "bzsuj7m6", // you can find this in sanity.json
-  dataset: "production", // or the name you chose in step 1
-  useCdn: false, // `false` if you want to ensure fresh data
+  projectId: 'bzsuj7m6', // you can find this in sanity.json
+  dataset: 'production', // or the name you chose in step 1
+  useCdn: false // `false` if you want to ensure fresh data
 });
