76adabec864af05a513749395917f7fb836652bc
Daniel Thorsen <daniel.rickard.thorsen@gmail.com>
2020-09-09 17:17:47 +0200

    Added pages

--- web/components/Header.js
+++ web/components/Header.js
@@ -1,9 +1,19 @@
 import Menu from "./Menu";
+import imageUrlBuilder from '@sanity/image-url';
+import client from "../client";
+
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source);
+}
 
 const Header = ({ props }) => {
   return (
     <>
+        {console.log(props.heroImage)}
+
       <Menu props={props} />
+      <img src={urlFor(props.heroImage.asset).width(200).url()} />
+
     </>
   );
 };
