603062dc74cf72bfdb6d56352c9741adc714cf3e
Daniel Thorsen <daniel.rickard.thorsen@gmail.com>
2020-09-23 15:18:25 +0200

    Removed focus/active, fixed image-hero bug and added info to mobilmenu

--- web/components/Events.js
+++ web/components/Events.js
@@ -7,16 +7,24 @@ function urlFor(source) {
   return imageUrlBuilder(client).image(source);
 }
 const Events = ({ props }) => {
+
+
   return (
     <section className={styles.eventsSection}>
-        {props.map((event, i) => {
-          return (
-            <div className={styles.eventCard}>
-            <img src={urlFor(event.mainImage.asset).url()} alt={event.eventname} />
-          <BodyContentBlock heading={event.eventname} paragraphArray={event.body} />
+      {props.map((event, i) => {
+        return (
+          <div className={styles.eventCard}>
+            <img
+              src={urlFor(event.mainImage.asset).url()}
+              alt={event.eventname}
+            />
+            <BodyContentBlock
+              heading={event.eventname}
+              paragraphArray={event.body}
+            />
           </div>
-          )
-        })}
+        );
+      })}
     </section>
   );
 };
